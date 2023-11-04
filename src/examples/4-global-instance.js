a80a446397635d6e20d26cf36bb5c6a01184c2bf
Jeff Lagarde <lagarde.marcjoseph@gmail.com>
2023-11-04 22:08:11 +0800

    axios global instance config

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 
 const productsUrl = 'https://course-api.com/react-store-products';
@@ -5,13 +6,20 @@ const randomUserUrl = 'https://randomuser.me/api';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try {
+      const res1 = await axios(productsUrl);
+      const res2 = await axios(randomUserUrl);
+      console.log(res1);
+      console.log(res2);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>global instance</h2>;
+  return <h2 className="text-center">global instance</h2>;
 };
 export default GlobalInstance;
